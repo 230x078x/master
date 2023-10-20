bd06831fe0e4bb807e692c7124c3afb8a14b42f2
rintaro <rintro090722@gmail.com>
2023-10-20 10:40:43 +0900

    pdb編集

--- editData/editPDB/insert_group.py
+++ editData/editPDB/insert_group.py
@@ -1,13 +1,13 @@
-with open('../dat/dock/dock2/1brs.pdb', 'r') as f:
+with open('./../../1BRS/1brs_cg_sep.pdb', 'r') as f:
     lines = f.readlines()
 
 new_lines = []
 for i, line in enumerate(lines):
-    if i < 1700:
+    if i < 108:
         new_line = line[:21] + 'A' + line[22:]
     else:
         new_line = line[:21] + 'B' + line[22:]
     new_lines.append(new_line)
 
-with open('../dat/dock/dock2/1brs_AB.pdb', 'w') as f:
+with open('./../../1BRS/1brs_cg_sep_AB.pdb', 'w') as f:
     f.writelines(new_lines)
