93dedbc7961256a98d7f30acf04bd0f6aa863963
rintaro <rintro090722@gmail.com>
2023-10-28 19:58:57 +0900

    black

--- editData/editPDB/insert_group.py
+++ editData/editPDB/insert_group.py
@@ -1,13 +1,13 @@
-with open('./../../1BRS/1brs_sep.pdb', 'r') as f:
+with open("./../../1BRS/1brs_sep.pdb", "r") as f:
     lines = f.readlines()
 
 new_lines = []
 for i, line in enumerate(lines):
     if i < 1704:
-        new_line = line[:21] + 'A' + line[22:]
+        new_line = line[:21] + "A" + line[22:]
     else:
-        new_line = line[:21] + 'B' + line[22:]
+        new_line = line[:21] + "B" + line[22:]
     new_lines.append(new_line)
 
-with open('./../../1BRS/1brs_sep_AB.pdb', 'w') as f:
+with open("./../../1BRS/1brs_sep_AB.pdb", "w") as f:
     f.writelines(new_lines)
